c1bed0e936dcafbaf6987980e0d4a164fece5590
Cristian David Castaño Giraldo - Ceiba Software <cristian.castano@ceiba.com.co>
2018-08-30 08:18:27 -0500

    Se crea la arquitectura del proyecto de reportes

---
+++ src/main/java/com/reporte/estacionamiento/reporte/ReporteApplication.java
@@ -0,0 +1,12 @@
+package com.reporte.estacionamiento.reporte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReporteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReporteApplication.class, args);
+	}
+}
